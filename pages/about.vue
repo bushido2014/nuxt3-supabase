4b1cad4961cc5492dbb9da9aebce1494f48849ec
bushido2014 <joomjoom2011@gmail.com>
2023-06-22 12:14:14 +0300

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h2>About Page</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet rerum
+      quos et!
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth'],
+});
+useHead({
+  title: 'About',
+  meta: [{ name: 'description', content: 'My amazing site.' }],
+  bodyAttrs: {
+    class: 'about-page',
+  },
+});
+</script>
